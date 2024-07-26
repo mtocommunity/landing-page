52f053e3ef72d5def3370a76afe03a03b88fc26f
Luis Bazán <luis@bazan.pe>
2024-07-25 20:01:38 -0500

    Add react to the project and add mto_transparent_background.svg

---
+++ src/assets/index.tsx
@@ -0,0 +1,17 @@
+export function MTOTransparentBackground({
+  className,
+}: {
+  className?: string;
+}) {
+  return (
+    <img
+      src="/mto/mto_transparent_background.svg"
+      alt="mto_transparent_background.svg"
+      className={className}
+    />
+  );
+}
+
+export function MTOLogo() {
+  return <img src="/mto/mto-02.svg" alt="mto_transparent_background.svg" />;
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        mto_gray: "#101B37",
+        mto_red: "#E41476",
+        mto_blue: "#02C8ED",
+      },
+    },
+  },
+  plugins: [],
+};
