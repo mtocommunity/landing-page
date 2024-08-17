b1fca8392b32b3e96a49758cfb495e97ae374880
Luis Bazán <luis@bazan.pe>
2024-08-16 20:04:56 -0500

    Added button

---
+++ src/components/general/attoms/button/index.tsx
@@ -0,0 +1,30 @@
+import "./style.css";
+import { ButtonType } from "./types";
+
+type ButtonProps = {
+  type: ButtonType;
+  children: React.ReactNode;
+  onClick?: () => void;
+  link?: string;
+  className?: string;
+};
+
+function Button({ type, children, onClick, link, className }: ButtonProps) {
+  const buttonClass = `${className} button ${ButtonType[type].toLowerCase()}`;
+
+  if (link) {
+    return (
+      <a href={link} className={buttonClass}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,9 @@ export default {
       colors: {
         mto_gray: "#101B37",
         mto_red: "#E41476",
+        mto_red_light: "#EC458D",
         mto_blue: "#02C8ED",
+        mto_blue_light: "#4BD5F1",
         mto_dark_gray: "#05090C",
       },
     },
