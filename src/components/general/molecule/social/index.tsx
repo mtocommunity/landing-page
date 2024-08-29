62e89a0f79ecad0d140aa80dc294be5d121b4db1
Luis Bazán <luis@bazan.pe>
2024-08-28 21:25:57 -0500

    Add a target in social media links

--- src/components/general/molecule/social/index.tsx
+++ src/components/general/molecule/social/index.tsx
@@ -13,22 +13,42 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Social() {
   return (
     <div className="flex justify-between w-full items-center px-0 py-2">
-      <a href="https://www.facebook.com/mtocommunity" className="icon">
+      <a
+        href="https://www.facebook.com/mtocommunity"
+        className="icon"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faFacebookSquare} />
       </a>
-      <a href="https://www.instagram.com/mtocommunity" className="icon">
+      <a
+        href="https://www.instagram.com/mtocommunity"
+        className="icon"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faInstagram} />
       </a>
-      <a href="https://www.youtube.com/@mtocommunity" className="icon">
+      <a
+        href="https://www.youtube.com/@mtocommunity"
+        className="icon"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faYoutube} />
       </a>
-      <a href="https://www.twitch.tv/mtocommunity" className="icon">
+      <a
+        href="https://www.twitch.tv/mtocommunity"
+        className="icon"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faTwitch} />
       </a>
-      <a href="https://www.tiktok.com/@mtocommunity" className="icon">
+      <a
+        href="https://www.tiktok.com/@mtocommunity"
+        className="icon"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faTiktok} />
       </a>
-      <a href="https://x.com/mtocommunity" className="icon">
+      <a href="https://x.com/mtocommunity" className="icon" target="_blank">
         <FontAwesomeIcon icon={faXTwitter} />
       </a>
     </div>
