ded8746e05a555b158e4f5e535a17e802444076d
DevJorgito <jorgito.soh@gmail.com>
2024-09-22 14:07:18 -0500

    division on staff page

--- src/components/landing/molecule/leader_card/index.tsx
+++ src/components/landing/molecule/leader_card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css'
 
 interface LeaderCardProps {
   photo: string;
@@ -14,7 +15,11 @@ const LeaderCard: React.FC<LeaderCardProps> = ({ photo, name, role, link }) => {
   }
 
   return (
-    <a href={link} className="block w-2/3 md:w-2/3 lg:w-2/3 rounded-lg overflow-hidden shadow-lg text-center transform transition-transform duration-300 hover:scale-105">
+    <a 
+      href={link} 
+      className="block w-2/3 md:w-2/3 lg:w-2/3 rounded-md overflow-hidden 
+                    text-center transform transition-transform duration-300 
+                    hover:scale-105 team-card-shadow">
       <img src={photo} className="w-full h-80 object-cover"/>
       <div className="p-4 bg-mto_gray text-white">
         <h3 className="text-xl font-semibold mb-1">{name}</h3>
