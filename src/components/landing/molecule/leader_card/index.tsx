85615b6e06a8007ec111199eb8c0e1b29617ff04
DevJorgito <jorgito.soh@gmail.com>
2024-09-21 23:54:43 -0500

    config staff page

--- src/components/landing/molecule/leader_card/index.tsx
+++ src/components/landing/molecule/leader_card/index.tsx
@@ -16,7 +16,7 @@ const LeaderCard: React.FC<LeaderCardProps> = ({ photo, name, role, link }) => {
   return (
     <a href={link} className="block w-2/3 md:w-2/3 lg:w-2/3 rounded-lg overflow-hidden shadow-lg text-center transform transition-transform duration-300 hover:scale-105">
       <img src={photo} className="w-full h-80 object-cover"/>
-      <div className="p-4 bg-mto_gray_light text-white">
+      <div className="p-4 bg-mto_gray text-white">
         <h3 className="text-xl font-semibold mb-1">{name}</h3>
         <p className="text-gray-400 text-sm">{role}</p>
       </div>
