558ddd1c22810793bf16e5cf2abf58b9bbb40a4d
Luis Bazán <luis@bazan.pe>
2024-09-23 00:46:37 -0500

    Edit link of dev team

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -16,8 +16,7 @@ export function MTOLogo() {
   return <img src="/mto/mto-02.svg" alt="mto_transparent_background.svg" />;
 }
 
-const landing_dev =
-  "file:///D:/GoogleDrive/Uni/mto-community/branding/web/dev-team.jpg";
+const landing_dev = "https://cdn.mtocommunity.com/landing-dev-team.webp";
 const landing_net = "https://cdn.mtocommunity.com/landing-net-team.webp";
 const landing_iot = "https://cdn.mtocommunity.com/landing-iot-team.webp";
 const landing_os = "https://cdn.mtocommunity.com/landing-os-team.webp";
