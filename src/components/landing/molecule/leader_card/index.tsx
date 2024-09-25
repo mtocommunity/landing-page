d5a8cf4a90c86bdfafe046f4505d9969fb8df8e2
Luis Bazán <luis@bazan.pe>
2024-09-25 18:45:28 -0500

    Added a border in leader_card

--- src/components/landing/molecule/leader_card/index.tsx
+++ src/components/landing/molecule/leader_card/index.tsx
@@ -12,7 +12,7 @@ const LeaderCard: React.FC<LeaderCardProps> = ({ photo, name, role, link }) => {
   return (
     <a
       href={link}
-      className="block w-auto rounded-md overflow-hidden transform transition-transform duration-300 hover:scale-105 team-card-shadow"
+      className="block w-auto rounded-md overflow-hidden transform hover:scale-105 team-card-shadow"
     >
       <img src={photo} className="h-80 object-cover aspect-[3/4]" alt={photo} />
       <div className="w-auto p-4 bg-mto_gray text-white">
