51266e67a31990f2d30ee88e9dbe1ab2cfd67f82
Luis Bazán <luis@bazan.pe>
2024-11-02 12:36:52 -0500

    Init a newsletter logic paths and content collections

---
+++ src/content/config.ts
@@ -0,0 +1,34 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const authorsCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    username: z.string(),
+    name: z.string(),
+    bio: z.string(),
+    avatar: z.string(),
+    links: z
+      .array(z.object({ url: z.string(), title: z.string() }))
+      .default([]),
+  }),
+});
+
+const newsletterCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z
+      .array(z.enum(["iot", "dev", "net", "os", "sec", "mto"]))
+      .optional()
+      .default([]),
+    thumbnail: z.string().optional(),
+    authors: z.array(reference("author")),
+  }),
+});
+
+export const collections = {
+  author: authorsCollection,
+  newsletter: newsletterCollection,
+};
