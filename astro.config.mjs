096b5db0f06ba901323047572764c08df59289e3
Luis Bazán <luis@bazan.pe>
2024-11-02 10:32:25 -0500

    Added mdx to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mtocommunity.com",
   base: "",
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), mdx()]
 });
