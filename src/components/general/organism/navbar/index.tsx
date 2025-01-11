c789c282f4fc6a56fdf3e22251adf47ea90cbbad
DevJorgito <jorgito.soh@gmail.com>
2025-01-10 21:50:40 -0500

    change color and titles in navbar

--- src/components/general/organism/navbar/index.tsx
+++ src/components/general/organism/navbar/index.tsx
@@ -15,9 +15,8 @@ function NavBar() {
   return (
     <>
       <header
-        className={`flex justify-between items-center h-14 sticky top-0 px-4 transition-colors z-50 ${
-          !toggle ? "bg-mto_gray" : "bg-transparent"
-        } lg:bg-transparent lg:w-[95%] lg:mx-auto lg:top-6`}
+        className={`flex justify-between items-center h-14 sticky top-0 px-4 transition-colors z-50 ${!toggle ? "bg-mto_gray" : "bg-transparent"
+          } lg:bg-transparent lg:w-[95%] lg:mx-auto lg:top-6`}
       >
         <a className="h-full lg:h-[150%] aspect-square" href="/">
           <MTOTransparentBackground />
@@ -27,18 +26,17 @@ function NavBar() {
         </div>
         <nav className="hidden lg:flex nav-desktop">
           {/* Nav Desktop */}
-          <a href="/#home">Home</a>
-          <a href="/#about-us">About us</a>
-          <a href="/#teams">Teams</a>
-          {/*<a href="/#events">Events</a>*/}
-          <a href="/staff">Staff</a>
-          <a href="/#contactus">Contact us</a>
+          <a href="/#home">Inicio</a>
+          <a href="/#about-us">Nosotros</a>
+          <a href="/#teams">Equipos</a>
+          <a href="/#schedule">Agenda</a>
+          <a href="/#leaders">Líderes</a>
+          <a href="/#blog">Blog</a>
         </nav>
       </header>
       <div
-        className={`nav-mobile fixed w-full top-14 bg-mto_gray text-mto_blue_light z-20 flex-col items-center justify-between py-4 text-xl lg:hidden ${
-          toggle ? "hidden" : "flex"
-        }`}
+        className={`nav-mobile fixed w-full top-14 bg-mto_gray text-mto_blue_light z-20 flex-col items-center justify-between py-4 text-xl lg:hidden ${toggle ? "hidden" : "flex"
+          }`}
       >
         {/* Mobile nav */}
         <a href="/#home">Home</a>
