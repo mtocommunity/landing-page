c910f3785d81b72734cb293eecd65f1b0e286207
DevJorgito <jorgito.soh@gmail.com>
2025-01-10 22:03:25 -0500

    add font onest

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,6 +24,7 @@ export default {
         main_sans: ["Lemon Milk", "sans"],
         secondary_sans: ["Arial", "sans"],
         main_mono: ["JetBrains Mono", "monospace"],
+        onest: ["Onest", "sans-serif"],
       },
       boxShadow: {
         'custom-cyan': '4px 4px 6px -2px rgba(75, 213, 241, 0.5), -4px -4px 6px -2px rgba(75, 213, 241, 0.25)',
