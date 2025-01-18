b9c40ef9ca75880db35598dbedac832d4f36bea7
DevJorgito <jorgito.soh@gmail.com>
2025-01-17 21:52:30 -0500

    change opacity in social

--- src/components/general/molecule/social/index.tsx
+++ src/components/general/molecule/social/index.tsx
@@ -15,7 +15,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Social() {
   return (
-    <div className="flex justify-between w-full items-center px-0 py-2">
+    <div className="flex justify-between w-full items-center px-0 py-2 opacity-40">
       <a href="https://www.instagram.com/mtocommunity" className="icon" target="_blank">
         <FontAwesomeIcon icon={faInstagram} />
       </a>
