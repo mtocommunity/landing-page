5e3fb6601fed3dedefea09c43d4db6c87f86562c
Luis Bazan <me@luisb.space>
2025-06-23 19:13:44 -0500

    Add license on files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,7 @@
+/*!
+ * Copyright (c) 2025 ManyToOne Community
+ * Licensed under the GPL-3.0 or later License: https://github.com/mtocommunity/landing-page/blob/master/LICENSE.md
+ */
+
 export * from "./formats";
 export * from "./times";
